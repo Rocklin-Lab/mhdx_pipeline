4e70273f3970df1ed9b84f4b5b73b9c6389d564c
Wes Ludwig <rwl0960@quser24.quest.it.northwestern.edu>
2020-10-01 21:35:09 -0500

    fix typo in optimize tensors jobscript

--- workflow/scripts/main/optimize_paths.py
+++ workflow/scripts/main/optimize_paths.py
@@ -3,7 +3,7 @@ import sys
 import pandas as pd
 
 sys.path.append(os.getcwd()+"/workflow/scripts/auxiliary/")
-import LC_IM_MS_TensorAnalysis as ta
+import LC_IM_MS_TensorAnalysis as hx
 
 
 library_info=pd.read_csv(snakemake.input[0])
@@ -11,20 +11,20 @@ name = snakemake.wildcards['name']
 atc = hx.limit_read(snakemake.input[1])
 fatc = []
 for tp in atc:
-    fatc.append([ic for ic in tp if ic.baseline_peak_error/ic.baseline_auc < 0.2])
+    fatc.append([ic for ic in tp if ic.baseline_peak_error/ic.baseline_auc < 0.25])
 
-p1 = ta.PathOptimizer(name, fatc, library_info, timepoints = snakemake.config['timepoints'], n_undeut_runs = len(snakemake.config[0]), old_data_dir = snakemake.config['old_data_dir'])
+p1 = hx.PathOptimizer(name, fatc, library_info, timepoints = snakemake.config['timepoints'], n_undeut_runs = len(snakemake.config[0]), old_data_dir = snakemake.config['old_data_dir'])
 
 p1.optimize_paths()
 
 p1.bokeh_plot(snakemake.output[0])
 
 #save winner, runners, undeut_grounds, winner_scores, and rtdt com cvs
-ta.limit_write(p1.winner, snakemake.output[1])
-ta.limit_write(p1.runners, snakemake.output[2])
-ta.limit_write([p1.undeut_grounds, p1.undeut_ground_dot_products], snakemake.output[3])
-ta.limit_write(p1.winner_scores, snakemake.output[4])
-ta.limit_write([p1.rt_com_cv, p1.dt_com_cv], snakemake.output[5])
+hx.limit_write(p1.winner, snakemake.output[1])
+hx.limit_write(p1.runners, snakemake.output[2])
+hx.limit_write([p1.undeut_grounds, p1.undeut_ground_dot_products], snakemake.output[3])
+hx.limit_write(p1.winner_scores, snakemake.output[4])
+hx.limit_write([p1.rt_com_cv, p1.dt_com_cv], snakemake.output[5])
 
 
 
