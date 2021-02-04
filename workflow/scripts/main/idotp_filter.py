d13d0bff29032bd0dc291c85e4b5759a33fabfe0
Wes Ludwig <rwl0960@quser24.quest.it.northwestern.edu>
2021-02-04 17:52:06 -0600

    working idotp_check and idotp_filter

--- workflow/scripts/main/idotp_filter.py
+++ workflow/scripts/main/idotp_filter.py
@@ -1,16 +1,8 @@
-print("0")
-import copy
-import ipdb
 import pandas as pd
-print("1")
 filtered_library_info = pd.read_csv(snakemake.input.pop(0))
-print("2")
-ins = copy.copy(snakemake.input)
-for fn in ins:
+for fn in snakemake.input:
 	lib_idx = int(fn.split('/')[-1].split('_')[0])
-	try:
-		if pd.read_csv(fn)["idotp"] < config["idotp_cutoff"]:
-			filtered_library_info.drop(lib_idx)
-	except:
-		ipdb.set_trace()
+	idpc = pd.read_csv(fn)
+	if idpc["idotp"].values[0] < snakemake.config["idotp_cutoff"]:
+		filtered_library_info.drop(lib_idx)
 filtered_library_info.to_csv(snakemake.output[0])
