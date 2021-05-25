bd0d9977c26e9946bf72dba6e60c40537e3c66db
Sugyan Dixit <dixitsugyan@gmail.com>
2021-05-25 12:14:26 -0500

    added ic plotting functions in generate_tensor_ics.py

--- workflow/scripts/HDX_LIMIT/pipeline/generate_tensor_ics.py
+++ workflow/scripts/HDX_LIMIT/pipeline/generate_tensor_ics.py
@@ -5,10 +5,131 @@ import psutil
 import numpy as np
 import argparse
 import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from matplotlib.backends.backend_pdf import PdfPages
 
 sys.path.append(os.getcwd() + "/workflow/scripts/")
 from HDX_LIMIT.processing import TensorGenerator, generate_tensor_factors
 from HDX_LIMIT.io import limit_write
+
+
+
+def convert_factor_mz_data_to_cont(mz_data, mz_labels, bins_per_isotope_peak):
+    padded_mz_labels = []
+    padded_factor_mz = []
+    mz_label_spacing = mz_labels[1] - mz_labels[0]
+
+    for i in range(0, len(mz_labels), bins_per_isotope_peak):
+        padded_mz_labels.append(mz_labels[i] - mz_label_spacing)
+        padded_mz_labels += list(mz_labels[i:i + bins_per_isotope_peak])
+        padded_mz_labels.append(padded_mz_labels[-1] + mz_label_spacing)
+
+        padded_factor_mz.append(0)
+        padded_factor_mz += list(mz_data[i:i + bins_per_isotope_peak])
+        padded_factor_mz.append(0)
+
+    return padded_mz_labels, padded_factor_mz
+
+
+def plot_mz_data(fig, gs, row_num, col_num, mz_label, mz_data, plot_label):
+
+    mz_sum = np.round(np.sum(mz_data), 2)
+
+    ax = fig.add_subplot(gs[row_num, col_num])
+    plt.plot(mz_label, mz_data, linewidth=0.5, label=plot_label)
+    plt.text(0, 0.1, s=str(mz_sum), fontsize=10, transform=ax.transAxes)
+    ax.set_yticks([])
+    ax.tick_params(length=3, pad=3)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    plt.legend(loc='best', fontsize='small')
+
+
+def plot_ics(list_of_ics_from_a_factor):
+
+
+    num_of_mz_plots = len(list_of_ics_from_a_factor) + 1
+
+    num_columns = 3
+    num_rows = 0
+
+    for num in range(num_of_mz_plots):
+        if num % num_columns == 0:
+            num_rows += 1
+
+
+    pad_factor_mz_label, pad_factor_mz = convert_factor_mz_data_to_cont(
+        mz_data=list_of_ics_from_a_factor[0].factor_mz_data,
+        mz_labels=list_of_ics_from_a_factor[0].mz_labels,
+        bins_per_isotope_peak=list_of_ics_from_a_factor[0].bins_per_isotope_peak)
+
+    fig = plt.figure(figsize=(15, num_rows * 1.6))
+    gs = gridspec.GridSpec(ncols=num_columns, nrows=num_rows, figure=fig)
+
+    n_rows = 0
+    n_cols = 0
+
+    # plot factor mz data
+    plot_mz_data(fig=fig,
+                 gs=gs,
+                 row_num=n_rows,
+                 col_num=n_cols,
+                 mz_label=pad_factor_mz_label,
+                 mz_data=pad_factor_mz,
+                 plot_label='factor_mz')
+
+
+    # plot ics from the factor
+    n_cols = 1
+
+    for num, ic in enumerate(list_of_ics_from_a_factor):
+
+        ic_mz_label, ic_mz_ = convert_factor_mz_data_to_cont(mz_data=ic.cluster_mz_data,
+                                                             mz_labels=ic.mz_labels,
+                                                             bins_per_isotope_peak=ic.bins_per_isotope_peak)
+
+        plot_mz_data(fig=fig,
+                     gs=gs,
+                     row_num=n_rows,
+                     col_num=n_cols,
+                     mz_label=ic_mz_label,
+                     mz_data=ic_mz_,
+                     plot_label=str(num))
+
+        if (n_cols+1) % num_columns == 0:
+            n_rows += 1
+            n_cols = 0
+        else:
+            n_cols += 1
+
+    title = 'Factor num %s' % list_of_ics_from_a_factor[0].factor_idx
+
+    plt.suptitle(title)
+    plt.tight_layout()
+
+
+def plot_ics_from_ic_list(list_of_ics, output_path):
+
+    ### sort ics with factor number
+    factor_indices = [x.factor_idx for x in list_of_ics]
+    unique_factor_idx = np.unique(factor_indices)
+    sorted_list_ics = [[] for _ in range(len(unique_factor_idx))]
+
+
+    for ind, factor_idx in enumerate(unique_factor_idx):
+        for ics in list_of_ics:
+            if ics.factor_idx == factor_idx:
+                sorted_list_ics[ind].append(ics)
+
+    ### plotting ics
+    with PdfPages(output_path) as pdf:
+        for ind, ic_list in enumerate(sorted_list_ics):
+            plot_ics(ic_list)
+
+            pdf.savefig()
+            plt.close()
 
 
 def main(library_info_path,
@@ -17,6 +138,7 @@ def main(library_info_path,
          isotope_clusters_out_path=None,
          factor_out_path=None,
          factor_plot_output_path=None,
+         ic_plot_output_path=None,
          return_flag=False,
          gauss_params=(3, 1),
          n_factors=15,
@@ -88,6 +210,9 @@ def main(library_info_path,
     if isotope_clusters_out_path is not None:
         limit_write(all_ics, isotope_clusters_out_path)
 
+    if ic_plot_output_path is not None:
+        plot_ics_from_ic_list(all_ics, ic_plot_output_path)
+
     if return_flag:
         out_dict["TensorGenerator"] = data_tensor
         return out_dict
@@ -121,6 +246,10 @@ if __name__ == "__main__":
         "-po",
         "--factor_plot_out_path",
         help="path/to/output.cpickle.zlib.factor.pdf, FactorData Plot output .pdf")
+    parser.add_argument(
+        "-po_ic",
+        "--ic_plot_out_path",
+        help="path/to/output.cpickle.zlib.ic.pdf, ICData Plot output .pdf")
     parser.add_argument(
         "-r",
         "--return_flag",
@@ -165,6 +294,7 @@ if __name__ == "__main__":
          isotope_clusters_out_path=args.isotope_clusters_out_path,
          factor_out_path=args.factor_data_out_path,
          factor_plot_output_path=args.factor_plot_out_path,
+         ic_plot_output_path=args.ic_plot_out_path,
          return_flag=args.return_flag,
          gauss_params=args.gauss_params,
          filter_factors=filter_factors,
